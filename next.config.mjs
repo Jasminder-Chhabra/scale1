da2e7b942a5490f670c2e2c68a4acbfd71421a12
adarsh jain <adarshj0304@gmail.com>
2025-08-07 12:54:27 +0530

    fix: update eslint configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: false, // Ensure ESLint runs during builds
+    // Optionally, specify rules to ignore
+    // This is not recommended for long-term use
+  },
   webpackDevMiddleware: config => {
     config.watchOptions = {
       poll: 1000,
